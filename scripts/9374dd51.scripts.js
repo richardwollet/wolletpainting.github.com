b05b3f56665b6cebe1745da9b4f68be03e0ee4ba
Scott Wade <sc0ttwad3@gmail.com>
2014-04-11 17:08:58 -0400

    Latest build/generated 4.11.14 @ 5:08pm

--- scripts/9374dd51.scripts.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("wolletpaintinggithubcomApp",["ngCookies","ngResource","ngSanitize","ngRoute"]),angular.module("wolletpaintinggithubcomApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
